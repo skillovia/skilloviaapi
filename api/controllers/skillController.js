588a9beb9e4bb1ca8b6bd31bff30c352eb2c3194
olaniyihope2 <olaniyihope2@gmail.com>
2024-12-08 22:59:03 +0100

    all

--- api/controllers/skillController.js
+++ api/controllers/skillController.js
@@ -1,41 +1,200 @@
-const Skill = require("../models/Skill")
+const Skill = require("../models/Skill");
 
 exports.createSkill = async (req, res) => {
-    const userId = req.user.id;
-    const data = req.body;
-  
-    try {
-      const skill = await Skill.createSkill(userId, data);
-      res.status(200).json({ status: 'success', message: 'Skill created successfully.', data: skill });
-    } catch (error) {
-      res.status(500).json({status: 'error', message: 'Failed to create skill.' });
-    }
-};
+  const userId = req.user.id;
+  const data = req.body;
 
+  try {
+    const skill = await Skill.createSkill(userId, data);
+    res
+      .status(200)
+      .json({
+        status: "success",
+        message: "Skill created successfully.",
+        data: skill,
+      });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ status: "error", message: "Failed to create skill." });
+  }
+};
 
 exports.updateSkill = async (req, res) => {
+  const userId = req.user.id;
+  const updates = req.body;
+  const skillId = parseInt(req.params.id);
+
+  try {
+    const skill = await Skill.update(userId, skillId, updates);
+    res
+      .status(200)
+      .json({
+        status: "success",
+        message: "Skill updated successfully.",
+        data: skill,
+      });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ status: "error", message: "Failed to update skill." });
+  }
+  const Skill = require("../models/Skill");
+
+  exports.createSkill = async (req, res) => {
+    const userId = req.user.id;
+    const data = req.body;
+
+    try {
+      const skill = await Skill.createSkill(userId, data);
+      res
+        .status(200)
+        .json({
+          status: "success",
+          message: "Skill created successfully.",
+          data: skill,
+        });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ status: "error", message: "Failed to create skill." });
+    }
+  };
+
+  exports.updateSkill = async (req, res) => {
     const userId = req.user.id;
     const updates = req.body;
-    const skillId = parseInt(req.params.id) 
-  
+    const skillId = parseInt(req.params.id);
+
     try {
       const skill = await Skill.update(userId, skillId, updates);
-      res.status(200).json({ status: 'success', message: 'Skill updated successfully.', data: skill });
+      res
+        .status(200)
+        .json({
+          status: "success",
+          message: "Skill updated successfully.",
+          data: skill,
+        });
     } catch (error) {
-      res.status(500).json({status: 'error', message: 'Failed to update skill.' });
+      res
+        .status(500)
+        .json({ status: "error", message: "Failed to update skill." });
     }
-};
+  };
 
-
-
-exports.deleteSkill = async (req, res) => {
+  exports.deleteSkill = async (req, res) => {
     const userId = req.user.id;
-    const skillId = parseInt(req.params.id)   
-  
+    const skillId = parseInt(req.params.id);
+
     try {
       const skill = await Skill.delete(userId, skillId);
-      res.status(200).json({ status: 'success', message: 'Skill deleted successfully.', data: skill });
+      res
+        .status(200)
+        .json({
+          status: "success",
+          message: "Skill deleted successfully.",
+          data: skill,
+        });
     } catch (error) {
-      res.status(500).json({status: 'error', message: 'Failed to delete skill.' });
+      res
+        .status(500)
+        .json({ status: "error", message: "Failed to delete skill." });
     }
+  };
+
+  exports.retrievePublishedSkill = async (req, res) => {
+    const status = "published";
+
+    try {
+      const skill = await Skill.retrievePublishedSkill(status);
+      res
+        .status(200)
+        .json({
+          status: "success",
+          message: "published skills retrieved successfully.",
+          data: skill,
+        });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ status: "error", message: "Failed to retrieve skills" });
+    }
+  };
+
+  exports.searchSkillsByName = async (req, res) => {
+    const searchTerm = req.params.term;
+
+    try {
+      const skill = await Skill.searchSkillsByName(searchTerm);
+      res
+        .status(200)
+        .json({
+          status: "success",
+          message: "published skills retrieved successfully.",
+          data: skill,
+        });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ status: "error", message: "Failed to retrieve skills" });
+    }
+  };
+
+  exports.searchSkillsByCreatorName = async (req, res) => {
+    const searchTerm = req.params.term;
+
+    try {
+      const skill = await Skill.searchSkillsByCreatorName(searchTerm);
+      res
+        .status(200)
+        .json({
+          status: "success",
+          message: "published skills retrieved successfully.",
+          data: skill,
+        });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ status: "error", message: "Failed to retrieve skills" });
+    }
+  };
+
+  exports.searchSkillsBySparktoken = async (req, res) => {
+    const searchTerm = req.params.term;
+
+    try {
+      const skill = await Skill.searchSkillsBySparktoken(searchTerm);
+      res
+        .status(200)
+        .json({
+          status: "success",
+          message: "published skills retrieved successfully.",
+          data: skill,
+        });
+    } catch (error) {
+      res
+        .status(500)
+        .json({ status: "error", message: "Failed to retrieve skills" });
+    }
+  };
+};
+
+exports.deleteSkill = async (req, res) => {
+  const userId = req.user.id;
+  const skillId = parseInt(req.params.id);
+
+  try {
+    const skill = await Skill.delete(userId, skillId);
+    res
+      .status(200)
+      .json({
+        status: "success",
+        message: "Skill deleted successfully.",
+        data: skill,
+      });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ status: "error", message: "Failed to delete skill." });
+  }
 };
