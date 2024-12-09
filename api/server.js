190df8ebc71d7833e163c898fd786f020b07adb1
olaniyihope2 <olaniyihope2@gmail.com>
2024-12-09 04:00:05 +0100

    all

--- api/server.js
+++ api/server.js
@@ -23,4 +23,4 @@ app.use("/api/follows", followRoutes);
 
 // Server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on this port ${PORT}`));
