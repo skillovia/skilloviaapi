d1e958ffd2cd790efb1a9f33508edb6ed6f2a028
OlaniyiHope <olaniyihoppee@gmail.com>
2025-03-14 11:16:42 +0100

    all

--- api/models/User.js
+++ api/models/User.js
@@ -1,24 +1,47 @@
-const { stat } = require('fs/promises');
-const pool = require('../config/db');
-const bcrypt = require('bcrypt');
+const { stat } = require("fs/promises");
+const pool = require("../config/db");
+const bcrypt = require("bcrypt");
 
 class User {
   static async create(data) {
-    const {phone, email, firstname, lastname, gender, password, referred_by} = data;
+    const { phone, email, firstname, lastname, gender, password, referred_by } =
+      data;
     const hashedPassword = await bcrypt.hash(password, 10);
     const code = Math.floor(1000 + Math.random() * 900000);
-    const referralCode = firstname + code
+    const referralCode = firstname + code;
 
     const result = await pool.query(
-      'INSERT INTO users (phone, email, firstname, lastname, gender, password, is_email_verified, referral_code, referred_by) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *',
-      [phone, email, firstname, lastname, gender, hashedPassword, 1, referralCode, referred_by]
+      "INSERT INTO users (phone, email, firstname, lastname, gender, password, is_email_verified, referral_code, referred_by) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *",
+      [
+        phone,
+        email,
+        firstname,
+        lastname,
+        gender,
+        hashedPassword,
+        1,
+        referralCode,
+        referred_by,
+      ]
     );
     return result.rows[0];
   }
 
-
   static async update(userId, updates) {
-    const { email, firstname, lastname, gender, password, location, street, zip_code, lat, lon, referred_by, website } = updates;
+    const {
+      email,
+      firstname,
+      lastname,
+      gender,
+      password,
+      location,
+      street,
+      zip_code,
+      lat,
+      lon,
+      referred_by,
+      website,
+    } = updates;
     const hashedPassword = password ? await bcrypt.hash(password, 10) : null;
 
     const result = await pool.query(
@@ -36,65 +59,67 @@ class User {
            referred_by = COALESCE($11, referred_by),
            website = COALESCE($12, website)
        WHERE id = $13 RETURNING *`,
-      [email, firstname, lastname, gender, hashedPassword, location, street, zip_code, lat, lon, referred_by, website, userId]
+      [
+        email,
+        firstname,
+        lastname,
+        gender,
+        hashedPassword,
+        location,
+        street,
+        zip_code,
+        lat,
+        lon,
+        referred_by,
+        website,
+        userId,
+      ]
     );
     return result.rows[0];
   }
-
 
   static async findByPhone(phone) {
-    const result = await pool.query(
-      'SELECT * FROM users WHERE phone = $1',
-      [phone]
-    );
+    const result = await pool.query("SELECT * FROM users WHERE phone = $1", [
+      phone,
+    ]);
     return result.rows[0];
   }
-
 
   static async findByEmail(email) {
-    const result = await pool.query(
-      'SELECT * FROM users WHERE email = $1',
-      [email]
-    );
+    const result = await pool.query("SELECT * FROM users WHERE email = $1", [
+      email,
+    ]);
     return result.rows[0];
   }
-
 
   static async findById(id) {
-    const result = await pool.query(
-      'SELECT * FROM users WHERE id = $1',
-      [id]
-    );
+    const result = await pool.query("SELECT * FROM users WHERE id = $1", [id]);
     return result.rows[0];
   }
-
 
   static async checkUserExist(phone, email) {
     const result = await pool.query(
-      'SELECT * FROM users WHERE phone = $1 OR email = $2',
+      "SELECT * FROM users WHERE phone = $1 OR email = $2",
       [phone, email]
     );
     return result.rows[0];
   }
 
-
   static async storeRefreshToken(token) {
     const result = await pool.query(
-      'INSERT INTO refreshtoken (token) VALUES ($1) RETURNING token',
+      "INSERT INTO refreshtoken (token) VALUES ($1) RETURNING token",
       [token]
     );
     return result.rows[0];
   }
-
 
   static async getRefreshToken(token) {
     const result = await pool.query(
-      'SELECT * FROM refreshtoken WHERE token = $1',
+      "SELECT * FROM refreshtoken WHERE token = $1",
       [token]
     );
     return result.rows[0];
   }
-
 
   static async resetPassword(userId, password) {
     const result = await pool.query(
@@ -106,7 +131,6 @@ class User {
     return result.rows[0];
   }
 
-
   static async changeNotificationType(userId, type) {
     const result = await pool.query(
       `UPDATE users 
@@ -117,7 +141,6 @@ class User {
     return result.rows[0];
   }
 
-
   static async changeAppearanceMode(userId, mode) {
     const result = await pool.query(
       `UPDATE users 
@@ -127,7 +150,6 @@ class User {
     );
     return result.rows[0];
   }
-
 
   /*  static async getProfileByUserId(id) {
     const result = await pool.query(
@@ -163,53 +185,98 @@ class User {
     return result.rows;
   } */
 
+  // static async getProfileByUserId(id) {
+  //   const result = await pool.query(
+  //       `
+  //       SELECT
+  //           users.*,
+
+  //           COALESCE(
+  //               JSON_AGG(
+  //                   JSON_BUILD_OBJECT(
+  //                       'skill_id', skills.id,
+  //                       'description', skills.description,
+  //                       'skill_type', skills.skill_type,
+  //                       'experience_level', skills.experience_level,
+  //                       'hourly_rate', skills.hourly_rate,
+  //                       'thumbnail01', skills.thumbnail01,
+  //                       'thumbnail02', skills.thumbnail02,
+  //                       'thumbnail03', skills.thumbnail03,
+  //                       'thumbnail04', skills.thumbnail04
+  //                   )
+  //               ) FILTER (WHERE skills.id IS NOT NULL),
+  //               '[]'
+  //           ) AS skills,
+
+  //           COALESCE(account.spark_token_balance, 0) AS spark_token_balance,
+  //           COALESCE(account.cash_balance, 0) AS cash_balance,
+
+  //           (SELECT COUNT(*) FROM follows WHERE follows.following_id = users.id) AS total_followers,
+  //           (SELECT COUNT(*) FROM follows WHERE follows.follower_id = users.id) AS total_following
+
+  //       FROM users
+  //       LEFT JOIN skills ON users.id = skills.user_id
+  //       LEFT JOIN account ON users.id = account.user_id
+  //       WHERE users.id = $1
+
+  //       GROUP BY users.id, account.spark_token_balance, account.cash_balance;
+  //       `,
+  //       [id]
+  //   );
+  //   return result.rows;
+  // }
 
   static async getProfileByUserId(id) {
     const result = await pool.query(
-        `
-        SELECT 
-            users.*,
-
-            COALESCE(
-                JSON_AGG(
-                    JSON_BUILD_OBJECT(
-                        'skill_id', skills.id,
-                        'description', skills.description,
-                        'skill_type', skills.skill_type,
-                        'experience_level', skills.experience_level,
-                        'hourly_rate', skills.hourly_rate,
-                        'thumbnail01', skills.thumbnail01,
-                        'thumbnail02', skills.thumbnail02,
-                        'thumbnail03', skills.thumbnail03,
-                        'thumbnail04', skills.thumbnail04
-                    )
-                ) FILTER (WHERE skills.id IS NOT NULL), 
-                '[]'
-            ) AS skills,
-
-            COALESCE(account.spark_token_balance, 0) AS spark_token_balance,
-            COALESCE(account.cash_balance, 0) AS cash_balance,
-
-            (SELECT COUNT(*) FROM follows WHERE follows.following_id = users.id) AS total_followers,
-            (SELECT COUNT(*) FROM follows WHERE follows.follower_id = users.id) AS total_following
-
-        FROM users
-        LEFT JOIN skills ON users.id = skills.user_id
-        LEFT JOIN account ON users.id = account.user_id
-        WHERE users.id = $1
-
-        GROUP BY users.id, account.spark_token_balance, account.cash_balance;
-        `,
-        [id]
+      `
+      SELECT 
+          users.id, users.phone, users.email, users.firstname, users.lastname, 
+          users.gender, users.notification_type, users.appearance_mode, 
+          users.photourl, users.bio, users.location, users.street, users.zip_code, 
+          users.created_at, users.updated_at, users.referral_code, users.website,
+  
+          COALESCE(
+              JSON_AGG(
+                  JSON_BUILD_OBJECT(
+                      'skill_id', skills.id,
+                      'description', skills.description,
+                      'skill_type', skills.skill_type,
+                      'experience_level', skills.experience_level,
+                      'hourly_rate', skills.hourly_rate,
+                      'thumbnail01', skills.thumbnail01,
+                      'thumbnail02', skills.thumbnail02,
+                      'thumbnail03', skills.thumbnail03,
+                      'thumbnail04', skills.thumbnail04
+                  )
+              ) FILTER (WHERE skills.id IS NOT NULL), 
+              '[]'
+          ) AS skills,
+  
+          COALESCE(account.spark_token_balance, 0) AS spark_token_balance,
+          COALESCE(account.cash_balance, 0) AS cash_balance,
+  
+          (SELECT COUNT(*) FROM follows WHERE follows.following_id = users.id) AS total_followers,
+          (SELECT COUNT(*) FROM follows WHERE follows.follower_id = users.id) AS total_following
+  
+      FROM users
+      LEFT JOIN skills ON users.id = skills.user_id
+      LEFT JOIN account ON users.id = account.user_id
+      WHERE users.id = $1
+  
+      GROUP BY users.id, users.phone, users.email, users.firstname, users.lastname, 
+               users.gender, users.notification_type, users.appearance_mode, 
+               users.photourl, users.bio, users.location, users.street, users.zip_code, 
+               users.created_at, users.updated_at, users.referral_code, users.website, 
+               account.spark_token_balance, account.cash_balance;
+      `,
+      [id]
     );
     return result.rows;
   }
 
-
-
   static async getProfileByUserName(name) {
     const result = await pool.query(
-        `
+      `
         SELECT 
             users.*,
 
@@ -243,12 +310,10 @@ class User {
 
         GROUP BY users.id, account.spark_token_balance, account.cash_balance;
         `,
-        [`%${name}%`]
+      [`%${name}%`]
     );
     return result.rows;
   }
-    
-
 
   static async changeAvatar(userId, filepath) {
     const result = await pool.query(
@@ -260,9 +325,8 @@ class User {
     return result.rows[0];
   }
 
-
   static async updateBio(userId, data) {
-    const { bio, location, street, zip_code } = data
+    const { bio, location, street, zip_code } = data;
 
     const result = await pool.query(
       `UPDATE users 
@@ -276,24 +340,21 @@ class User {
     return result.rows[0];
   }
 
-
   static async verifyEmail(email, code) {
     const result = await pool.query(
-      'SELECT * FROM verifyemail WHERE email = $1 AND token = $2',
+      "SELECT * FROM verifyemail WHERE email = $1 AND token = $2",
       [email, code]
     );
     return result.rows[0];
   }
 
-
   static async insertVerificationCode(email, code) {
     const result = await pool.query(
-    'INSERT INTO verifyemail (email, token) VALUES ($1,$2) RETURNING *',
-    [email, code]
+      "INSERT INTO verifyemail (email, token) VALUES ($1,$2) RETURNING *",
+      [email, code]
     );
     return result.rows[0];
   }
-
 
   static async updateVerificationCode(email, code) {
     const result = await pool.query(
@@ -305,7 +366,6 @@ class User {
     return result.rows[0];
   }
 
-
   static async updateVerificationStatus(email, status) {
     const result = await pool.query(
       `UPDATE users 
@@ -316,27 +376,24 @@ class User {
     return result.rows[0];
   }
 
-
   static async checkVerificationExist(email) {
     const result = await pool.query(
-      'SELECT * FROM verifyemail WHERE email = $1',
+      "SELECT * FROM verifyemail WHERE email = $1",
       [email]
     );
     return result.rows[0];
   }
 
-
   static async deleteUser(email) {
     const result = await pool.query(
-        `DELETE FROM users 
+      `DELETE FROM users 
         WHERE email = $1 
         RETURNING *`,
-        [email]
+      [email]
     );
 
     return result.rows[0];
   }
-
 
   static async updateCordinates(lat, lng, radius, date, userId) {
     const result = await pool.query(
@@ -351,11 +408,9 @@ class User {
     return result.rows[0];
   }
 
-
   static async findNearbyUsers(lat, lon, radius = 5) {
-    
     const result = await pool.query(
-    `SELECT id, firstname, lastname, lat, lon, email, phone, gender, photourl,
+      `SELECT id, firstname, lastname, lat, lon, email, phone, gender, photourl,
         (
           6371 * acos(
             cos(radians($1)) * cos(radians(lat)) *
@@ -374,34 +429,28 @@ class User {
       ORDER BY distance;`,
       [lat, lon, radius]
     );
-    
-    return result.rows
+
+    return result.rows;
   }
-
-
 
   static async findNearbyUsersByAddress(address) {
     const result = await pool.query(
-        `
+      `
         SELECT 
           users.id, firstname, lastname, lat, lon, email, phone, gender, photourl
         FROM users 
         WHERE location ILIKE $1
         OR street ILIKE $1
         `,
-        [`%${address}%`]
+      [`%${address}%`]
     );
     return result.rows;
   }
-
 
   static async getAllusers() {
-    const result = await pool.query(
-      'SELECT * FROM users'
-    );
+    const result = await pool.query("SELECT * FROM users");
     return result.rows;
   }
-
 
   static async setReferralCode(userId, code) {
     const result = await pool.query(
@@ -413,83 +462,79 @@ class User {
     return result.rows[0];
   }
 
-
   static async getUsersByReferralCode(code) {
     const result = await pool.query(
-      'SELECT * FROM users WHERE referred_by = $1',
+      "SELECT * FROM users WHERE referred_by = $1",
       [code]
     );
     return result.rows[0];
   }
 
-
   static async storeResetPasswordToken(userId, token) {
     const result = await pool.query(
-      'INSERT INTO password_reset_tokens (user_id, token, expires_at) VALUES ($1, $2, NOW() + INTERVAL \'1 hour\') RETURNING token',
+      "INSERT INTO password_reset_tokens (user_id, token, expires_at) VALUES ($1, $2, NOW() + INTERVAL '1 hour') RETURNING token",
       [userId, token]
     );
     return result.rows[0];
   }
 
-
   static async getPasswordResetTokens(token, userId) {
     const result = await pool.query(
-      'SELECT * FROM password_reset_tokens WHERE token = $1 AND user_id = $2',
+      "SELECT * FROM password_reset_tokens WHERE token = $1 AND user_id = $2",
       [token, userId]
     );
     return result.rows[0];
   }
 
-
   static async deletePasswordResetTokens(token) {
     const result = await pool.query(
-        `DELETE FROM password_reset_tokens 
+      `DELETE FROM password_reset_tokens 
         WHERE token = $1`,
-        [token]
+      [token]
     );
 
     return result.rows[0];
   }
 
-
   static async getUserNotifications(userId) {
     const result = await pool.query(
-      'SELECT * FROM notifications WHERE user_id = $1',
+      "SELECT * FROM notifications WHERE user_id = $1",
       [userId]
     );
     return result.rows;
   }
 
-
   static async createStripeAccount(user_id, stripe_account_id) {
     const result = await pool.query(
-    'INSERT INTO stripe_account (user_id, stripe_account_id) VALUES ($1,$2) RETURNING *',
-    [user_id, stripe_account_id]
+      "INSERT INTO stripe_account (user_id, stripe_account_id) VALUES ($1,$2) RETURNING *",
+      [user_id, stripe_account_id]
     );
     return result.rows[0];
   }
 
-
   static async checkStripeAccountExist(user_id) {
     const result = await pool.query(
-      'SELECT * FROM stripe_account WHERE user_id = $1',
+      "SELECT * FROM stripe_account WHERE user_id = $1",
       [user_id]
     );
     return result.rows[0];
   }
 
-
   static async deleteStripeAccount(id) {
     const result = await pool.query(
-        `DELETE FROM stripe_account WHERE id = $1`,
-        [id]
+      `DELETE FROM stripe_account WHERE id = $1`,
+      [id]
     );
 
     return result.rows[0];
   }
 
-
-  static async updateStripeAccount(charges_enabled, payouts_enabled, details_submitted, stripe_account_id) {
+  static async updateStripeAccount(
+    charges_enabled,
+    payouts_enabled,
+    details_submitted,
+    stripe_account_id
+  ) {
     const result = await pool.query(
       `UPDATE stripe_account 
       SET charges_enabled = COALESCE($1, charges_enabled),
@@ -500,8 +545,6 @@ class User {
     );
     return result.rows[0];
   }
-
 }
-
 
 module.exports = User;
