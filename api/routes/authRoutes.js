db82b30dd55f50072dbfba8868b340317e72ab60
OlaniyiHope <olaniyihoppee@gmail.com>
2025-03-20 08:01:50 +0100

    all

--- api/routes/authRoutes.js
+++ api/routes/authRoutes.js
@@ -1,42 +1,58 @@
-const express = require('express');
-const { registerUser, login, refreshToken, refreshTokenWeb, resetPassword, changePassword, verifyPhone, sendVerificationCode, verifyEmail, resendEmailVerirficationCode, adminDelete, forgotPassword} = require('../controllers/authController');
-const passport = require('passport');
+const express = require("express");
+const {
+  registerUser,
+  login,
+  refreshToken,
+  refreshTokenWeb,
+  resetPassword,
+  changePassword,
+  verifyPhone,
+  sendVerificationCode,
+  verifyEmail,
+  resendEmailVerirficationCode,
+  adminDelete,
+  forgotPassword,
+} = require("../controllers/authController");
+const passport = require("passport");
 const router = express.Router();
-const { getClientIp } = require('../middlewares/ipgetter');
+const { getClientIp } = require("../middlewares/ipgetter");
 
-
-router.post('/register', registerUser);
-router.post('/login', getClientIp, login);
-router.delete('/user/:email', adminDelete);
-router.get('/get/refreshtoken', refreshToken);
-router.get('/get/refreshtokenweb', refreshTokenWeb);
-router.put('/reset/password/:id', changePassword);
-router.put('/reset/password', changePassword);
-router.post('/sendverificationcode', sendVerificationCode);
-router.post('/verifyphone', verifyPhone);
-router.post('/verify/email', verifyEmail);
-router.post('/resend/code', resendEmailVerirficationCode);
-router.post('/forgot/password', forgotPassword);
-router.post('/reset/forgot/password', resetPassword);
-
+router.post("/register", registerUser);
+router.post("/login", getClientIp, login);
+router.delete("/user/:email", adminDelete);
+router.get("/get/refreshtoken", refreshToken);
+router.get("/get/refreshtokenweb", refreshTokenWeb);
+router.put("/reset/password/:id", changePassword);
+router.put("/reset/password", changePassword);
+router.post("/sendverificationcode", sendVerificationCode);
+router.post("/verifyphone", verifyPhone);
+router.post("/verify/email", verifyEmail);
+router.post("/resend/code", resendEmailVerirficationCode);
+router.post("/forgot/password", forgotPassword);
+router.post("/reset/forgot/password", resetPassword);
 
 // Redirect to Google for authentication
-router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get(
+  "/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
 
 // Callback route for Google to redirect to
 router.get(
-    '/google/callback',
-    passport.authenticate('google', { failureRedirect: '/login' }),
-    (req, res) => {
-        const token = req.user.token;
-        /* res.status(200).json({
+  "/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  (req, res) => {
+    const token = req.user.token;
+    /* res.status(200).json({
             status: 'success',
             message: 'Login was successful',
             data: {accessToken: token, refreshToken:token},
         }); */
 
-        res.redirect(`https://skilloviaweb.vercel.app/explore?accessToken=${token}&refreshToken=${token}`);
-    }
+    res.redirect(
+      `https://skilloviaweb.vercel.app/explore?accessToken=${token}&refreshToken=${token}`
+    );
+  }
 );
 
 module.exports = router;
