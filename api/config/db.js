84accb1bc59a71ae3df04297d480f8059b138aaf
LifeMirrorApp <lifemirrorapp@gmail.com>
2025-03-25 16:06:23 +0100

    all

--- api/config/db.js
+++ api/config/db.js
@@ -1,18 +1,18 @@
-// const { Pool } = require("pg");
-
-// const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-//   ssl: {
-//     rejectUnauthorized: false, // Required for AWS RDS SSL connections
-//   },
-// });
-
-// module.exports = pool;
 const { Pool } = require("pg");
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: false, // Disable SSL for local development
+  ssl: {
+    rejectUnauthorized: false, // Required for AWS RDS SSL connections
+  },
 });
 
 module.exports = pool;
+// const { Pool } = require("pg");
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: false, // Disable SSL for local development
+// });
+
+// module.exports = pool;
