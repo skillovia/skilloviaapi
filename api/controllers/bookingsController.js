dea356d481f770875e4a122993cfb30e0757951a
LifeMirrorApp <lifemirrorapp@gmail.com>
2025-03-26 13:06:25 +0100

    all

--- api/controllers/bookingsController.js
+++ api/controllers/bookingsController.js
@@ -1,52 +1,89 @@
-const Bookings = require('../models/Bookings');
+const Bookings = require("../models/Bookings");
 
+// exports.createBookings = async (req, res) => {
+//   const userId = req.user.id;
+//   const data = req.body;
+
+//   if(req.body.title != null){
+//     let file = null
+
+//     if(req.filePaths != null){
+//         const filePath = req.filePaths;
+//         file = filePath.slice(15);
+//     }
+
+//     try {
+//         const bookings = await Bookings.create(userId, data, file);
+//         res.status(200).json({ status: 'success', message: 'Skill booked successfully.', data: bookings });
+//     } catch (error) {
+//         res.status(500).json({status: 'error', message: 'Failed to book skills.', data: error });
+//     }
+
+//   } else {
+//     res.status(400).send({
+//         status: 'error',
+//         message: 'Title is required',
+//         data: null
+//     });
+//   }
+// };
 exports.createBookings = async (req, res) => {
   const userId = req.user.id;
   const data = req.body;
 
-  if(req.body.title != null){
-    let file = null
+  if (!data.title) {
+    return res.status(400).json({
+      status: "error",
+      message: "Title is required",
+      data: null,
+    });
+  }
 
-    if(req.filePaths != null){
-        const filePath = req.filePaths;
-        file = filePath.slice(15);
-    }
+  let fileUrl = null;
+  if (req.file) {
+    fileUrl = req.file.location; // AWS S3 returns the file URL in `req.file.location`
+  }
 
-    try {
-        const bookings = await Bookings.create(userId, data, file);
-        res.status(200).json({ status: 'success', message: 'Skill booked successfully.', data: bookings });
-    } catch (error) {
-        res.status(500).json({status: 'error', message: 'Failed to book skills.', data: error });
-    }
-
-  } else {
-    res.status(400).send({
-        status: 'error',
-        message: 'Title is required',
-        data: null
+  try {
+    const bookings = await Bookings.create(userId, data, fileUrl);
+    res.status(200).json({
+      status: "success",
+      message: "Skill booked successfully.",
+      data: bookings,
+    });
+  } catch (error) {
+    console.error("Error in createBookings:", error);
+    res.status(500).json({
+      status: "error",
+      message: "Failed to book skills.",
+      data: error,
     });
   }
 };
 
-
 exports.updateBookings = async (req, res) => {
-    const bookingsId = parseInt(req.params.id);
-    const data = req.body;
-  
-    let file = null
-    if(req.filePaths != null){
-        const filePath = req.filePaths;
-        file = filePath.slice(15);
-    }
-  
-    //try {
-        const bookings = await Bookings.update(bookingsId, data, file);
-        res.status(200).json({ status: 'success', message: 'Bookings updated successfully.', data: bookings });
-    /* } catch (error) {
+  const bookingsId = parseInt(req.params.id);
+  const data = req.body;
+
+  let file = null;
+  if (req.filePaths != null) {
+    const filePath = req.filePaths;
+    file = filePath.slice(15);
+  }
+
+  //try {
+  const bookings = await Bookings.update(bookingsId, data, file);
+  res
+    .status(200)
+    .json({
+      status: "success",
+      message: "Bookings updated successfully.",
+      data: bookings,
+    });
+  /* } catch (error) {
         res.status(500).json({status: 'error', message: 'Failed to update booking.', data: error });
     } */
 };
-
 
 exports.rejectBookings = async (req, res) => {
   const status = "rejected";
@@ -54,72 +91,128 @@ exports.rejectBookings = async (req, res) => {
 
   try {
     const data = await Bookings.changeStatus(bookingsId, status);
-    res.status(200).json({ status: 'success', message: 'Bookings rejected successfully.', data: data });
+    res
+      .status(200)
+      .json({
+        status: "success",
+        message: "Bookings rejected successfully.",
+        data: data,
+      });
   } catch (error) {
-    res.status(500).json({status: 'error', message: 'Failed to update status.', data: error });
+    res
+      .status(500)
+      .json({
+        status: "error",
+        message: "Failed to update status.",
+        data: error,
+      });
   }
 };
 
-
 exports.acceptBookings = async (req, res) => {
-    const status = "accepted";
-    const bookingsId = parseInt(req.params.id);
+  const status = "accepted";
+  const bookingsId = parseInt(req.params.id);
 
-    try {
-        const data = await Bookings.changeStatus(bookingsId, status);
-        res.status(200).json({ status: 'success', message: 'Bookings accepted successfully.', data: data });
-    } catch (error) {
-        res.status(500).json({status: 'error', message: 'Failed to update status.', data: error });
-    }
+  try {
+    const data = await Bookings.changeStatus(bookingsId, status);
+    res
+      .status(200)
+      .json({
+        status: "success",
+        message: "Bookings accepted successfully.",
+        data: data,
+      });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        status: "error",
+        message: "Failed to update status.",
+        data: error,
+      });
+  }
 };
-
-
 
 // get inward bookings
 exports.getInwardBookingsByUserId = async (req, res) => {
-    const userId = req.user.id;
-  
-    try {
-      const data = await Bookings.getInwardBookingsByUserId(userId);
-      if(data != null){
-        res.status(200).json({ status: 'success', message: 'User bookings retrieved successfully.', data: data });
-      } else {
-        res.status(200).json({ status: 'success', message: 'No bookings found', data: [] });
-      }
-      
-    } catch (error) {
-      res.status(500).json({status: 'error', message: 'Failed to retrieve bookings', data: error });
-    }
-};
-
-
-// get outward bookings
-exports.getOutwardBookingsByUserId = async (req, res) => {
-    const userId = req.user.id;
-  
-    try {
-      const data = await Bookings.getOutwardBookingsByUserId(userId);
-      if(data != null){
-        res.status(200).json({ status: 'success', message: 'User bookings retrieved successfully.', data: data });
-      } else {
-        res.status(200).json({ status: 'success', message: 'No bookings found', data: [] });
-      }
-      
-    } catch (error) {
-      res.status(500).json({status: 'error', message: 'Failed to retrieve bookings', data: error });
-    }
-};
-
-
-exports.removeBookings = async (req, res) => {
   const userId = req.user.id;
-  const id = parseInt(req.params.id)
 
   try {
-    const data = await Bookings.deleteBookings(userId, id);
-    res.status(200).json({ status: 'success', message: 'Bookings deleted successfully.', data: data });
+    const data = await Bookings.getInwardBookingsByUserId(userId);
+    if (data != null) {
+      res
+        .status(200)
+        .json({
+          status: "success",
+          message: "User bookings retrieved successfully.",
+          data: data,
+        });
+    } else {
+      res
+        .status(200)
+        .json({ status: "success", message: "No bookings found", data: [] });
+    }
   } catch (error) {
-  res.status(500).json({status: 'error', message: 'Failed to delete bookings.', data: error });
+    res
+      .status(500)
+      .json({
+        status: "error",
+        message: "Failed to retrieve bookings",
+        data: error,
+      });
   }
 };
 
+// get outward bookings
+exports.getOutwardBookingsByUserId = async (req, res) => {
+  const userId = req.user.id;
+
+  try {
+    const data = await Bookings.getOutwardBookingsByUserId(userId);
+    if (data != null) {
+      res
+        .status(200)
+        .json({
+          status: "success",
+          message: "User bookings retrieved successfully.",
+          data: data,
+        });
+    } else {
+      res
+        .status(200)
+        .json({ status: "success", message: "No bookings found", data: [] });
+    }
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        status: "error",
+        message: "Failed to retrieve bookings",
+        data: error,
+      });
+  }
+};
+
+exports.removeBookings = async (req, res) => {
+  const userId = req.user.id;
+  const id = parseInt(req.params.id);
+
+  try {
+    const data = await Bookings.deleteBookings(userId, id);
+    res
+      .status(200)
+      .json({
+        status: "success",
+        message: "Bookings deleted successfully.",
+        data: data,
+      });
+  } catch (error) {
+    res
+      .status(500)
+      .json({
+        status: "error",
+        message: "Failed to delete bookings.",
+        data: error,
+      });
+  }
+};
--- api/routes/bookingsRoutes.js
+++ api/routes/bookingsRoutes.js
@@ -1,43 +1,85 @@
-const express = require('express');
-const {createBookings, updateBookings, rejectBookings, acceptBookings,
-    getInwardBookingsByUserId, getOutwardBookingsByUserId, removeBookings
-} = require('../controllers/bookingsController');
+const express = require("express");
+const {
+  createBookings,
+  updateBookings,
+  rejectBookings,
+  acceptBookings,
+  getInwardBookingsByUserId,
+  getOutwardBookingsByUserId,
+  removeBookings,
+} = require("../controllers/bookingsController");
 const router = express.Router();
-const verify = require("../middlewares/verifyToken")
-const multer = require('multer');
-const fs = require('fs');
-const path = require('path');
+const verify = require("../middlewares/verifyToken");
+const multer = require("multer");
+const multerS3 = require("multer-s3");
+const { S3Client } = require("@aws-sdk/client-s3");
+const fs = require("fs");
+const path = require("path");
 
-const storage = multer.diskStorage({
-  destination: function (req, file, callback) {
-     const uploadDir = '/uploads/files';
-     
-     callback(null, __dirname + uploadDir);
+// const storage = multer.diskStorage({
+//   destination: function (req, file, callback) {
+//      const uploadDir = '/uploads/files';
+
+//      callback(null, __dirname + uploadDir);
+//   },
+//   filename: function (req, file, callback) {
+//      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+//      const filename = uniqueSuffix + path.extname(file.originalname);
+//      const filePath = path.join(__dirname + '/uploads/files', filename);
+
+//      // Store the file path in req
+//      if (!req.filePaths) {
+//         req.filePaths = null;
+//      }
+//      req.filePaths = filePath;
+
+//      callback(null, filename);
+//   }
+// });
+
+// const upload = multer({ storage: storage })
+
+// Initialize S3 Client
+const s3 = new S3Client({
+  region: process.env.AWS_REGION,
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
-  filename: function (req, file, callback) {
-     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-     const filename = uniqueSuffix + path.extname(file.originalname);
-     const filePath = path.join(__dirname + '/uploads/files', filename);
-
-     // Store the file path in req
-     if (!req.filePaths) {
-        req.filePaths = null;
-     }
-     req.filePaths = filePath;
-
-     callback(null, filename);
-  }
 });
 
-const upload = multer({ storage: storage })
+// Configure Multer to Upload to S3
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: "skillovia", // Your S3 bucket name
+    metadata: (req, file, cb) => {
+      cb(null, { fieldName: file.fieldname });
+    },
+    key: (req, file, cb) => {
+      const uniqueFilename = `uploads/booking/${Date.now()}-${
+        file.originalname
+      }`;
+      cb(null, uniqueFilename);
+    },
+  }),
+  limits: { fileSize: 10 * 1024 * 1024 }, // 10MB file size limit
+});
 
-router.post('/', verify, upload.single('file'), createBookings);
-router.put('/:id', verify, upload.single('file'), updateBookings);
-router.put('/reject/:id', verify, rejectBookings);
-router.put('/accept/:id', verify, acceptBookings);
-router.get('/get/user/inward', verify, getInwardBookingsByUserId);
-router.get('/get/user/outward', verify, getOutwardBookingsByUserId);
-router.delete('/:id', verify, removeBookings);
-
+// router.post('/', verify, upload.single('file'), createBookings);
+router.post("/", verify, upload.single("file"), async (req, res) => {
+  try {
+    await createBookings(req, res);
+  } catch (err) {
+    console.error("Error creating booking:", err);
+    res.status(500).json({ status: "error", message: "Internal Server Error" });
+  }
+});
+router.put("/:id", verify, upload.single("file"), updateBookings);
+router.put("/reject/:id", verify, rejectBookings);
+router.put("/accept/:id", verify, acceptBookings);
+router.get("/get/user/inward", verify, getInwardBookingsByUserId);
+router.get("/get/user/outward", verify, getOutwardBookingsByUserId);
+router.delete("/:id", verify, removeBookings);
 
 module.exports = router;
