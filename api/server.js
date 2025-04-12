e868ef6fcbd74b2c3f4f6a7e81c0e5e1dcd3decc
LifeMirrorApp <lifemirrorapp@gmail.com>
2025-04-12 11:45:41 +0100

    all

--- api/server.js
+++ api/server.js
@@ -58,8 +58,8 @@ app.use(
 );
 
 app.options("*", cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
