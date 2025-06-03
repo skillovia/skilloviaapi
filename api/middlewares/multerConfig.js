c671901ab1a70462a79a6c245271384b3193f3a7
LifeMirrorApp <lifemirrorapp@gmail.com>
2025-06-03 06:20:48 +0100

    all

--- api/middlewares/multerConfig.js
+++ api/middlewares/multerConfig.js
@@ -6,6 +6,7 @@ const path = require("path");
 // Initialize S3 client
 const s3 = new S3Client({
   region: process.env.AWS_REGION,
+  endpoint: `https://s3.${region}.amazonaws.com`,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
